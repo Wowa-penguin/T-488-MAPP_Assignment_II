fb7172a806d994737fc1a8096dc41b2640184384
borki1234 <bjarkihreinn@live.com>
2025-12-01 11:28:29 +0000

    Added Test File

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import React from 'react';
 
 export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+    );
 }
--- components/contact_detail.tsx
+++ components/contact_detail.tsx
@@ -1,37 +1,37 @@
-import * as Contacts from "expo-contacts";
-import React, { useEffect } from "react";
+import * as Contacts from 'expo-contacts';
+import React, { useEffect } from 'react';
 
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  useEffect(() => {
-    (async () => {
-      const { status } = await Contacts.requestPermissionsAsync();
-      if (status === "granted") {
-        const { data } = await Contacts.getContactsAsync({
-          fields: [Contacts.Fields.Emails],
-        });
+    useEffect(() => {
+        (async () => {
+            const { status } = await Contacts.requestPermissionsAsync();
+            if (status === 'granted') {
+                const { data } = await Contacts.getContactsAsync({
+                    fields: [Contacts.Fields.Emails],
+                });
 
-        if (data.length > 0) {
-          const contact = data[0];
-          console.log(contact);
-        }
-      }
-    })();
-  }, []);
+                if (data.length > 0) {
+                    const contact = data[0];
+                    console.log(contact);
+                }
+            }
+        })();
+    }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Contacts Module Example</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Contacts Module Example</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
